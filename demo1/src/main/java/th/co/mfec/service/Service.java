5b8bbd5415b03a97a044489381258cabeeeee91a
Suchansinee <nunnicee@gmail.com>
2019-03-25 17:17:25 +0700

    commit code

--- demo1/src/main/java/th/co/mfec/service/Service.java
+++ demo1/src/main/java/th/co/mfec/service/Service.java
@@ -8,5 +8,6 @@ public class Service {
 	public void helloWorld() {
 		System.out.println("****Demo1 Hello World!****");
 		System.out.println("****Hello Jenkins!****");
+		System.out.println("Hi nice!");
 	}
 }
