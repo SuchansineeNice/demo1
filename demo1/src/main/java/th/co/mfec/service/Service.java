17f801894acd2cb343a78c46e508b15e337cd6fe
Suchansinee <nunnicee@gmail.com>
2019-03-25 17:04:55 +0700

    update code

--- demo1/src/main/java/th/co/mfec/service/Service.java
+++ demo1/src/main/java/th/co/mfec/service/Service.java
@@ -7,6 +7,6 @@ public class Service {
 	
 	public void helloWorld() {
 		System.out.println("****Demo1 Hello World!****");
-		System.out.println("Hello Jenkins!");
+		System.out.println("****Hello Jenkins!****");
 	}
 }
